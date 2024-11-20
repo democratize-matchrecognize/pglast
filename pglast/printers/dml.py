ac9b696345d2bc30c376e5ea91708727c848f35e
Louisa Lambrecht <louisa.lambrecht@uni-tuebingen.de>
2024-11-20 11:15:03 +0100

    print query inside cte on new line and indented by 2 spaces

--- pglast/printers/dml.py
+++ pglast/printers/dml.py
@@ -419,7 +419,10 @@ def common_table_expr(node, output):
     # expression. The IndentedStream ignores the `force` argument.
     output.space(1, force=True)
     with output.expression(True):
-        output.print_node(node.ctequery)
+        output.newline()
+        with output.push_indent(amount=2, relative=False):
+            output.print_node(node.ctequery)
+        output.newline()
     if node.search_clause:
         output.newline()
         output.print_node(node.search_clause)
